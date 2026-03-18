24f867b535f35a691fa5864588d2c360809707f4
Erin Sparling <erin@everyplace.net>
2026-03-17 22:20:22 -0400

    Fix formula: head branch should be master not main

--- Formula/shpool.rb
+++ Formula/shpool.rb
@@ -4,7 +4,7 @@ class Shpool < Formula
   url "https://github.com/shell-pool/shpool/archive/refs/tags/v0.9.5.tar.gz"
   sha256 "cc958a1f66ed8c75892544c7921a764f0469233a0ca58b441249ed593bdcdaf0"
   license "Apache-2.0"
-  head "https://github.com/shell-pool/shpool.git", branch: "main"
+  head "https://github.com/shell-pool/shpool.git", branch: "master"
 
   depends_on "rust" => :build
 
